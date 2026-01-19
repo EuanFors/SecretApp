1a53858d44452832d5258fd79e0e5bb1e5366980
Euan Fors <euan.fors@learnet.se>
2026-01-19 13:57:10 +0100

    Added a functional menu with loop and if statements

--- SecretApp/Program.cs
+++ SecretApp/Program.cs
@@ -1,17 +1,41 @@
-﻿using System.ComponentModel.Design;
-using System.Net.Http.Json;
-
-namespace SecretApp1
+﻿namespace SecretApp
 {
     internal class Program
-    {
-
-        static string[] userNames = { "Pelle", "Stina", "Ali" };
-        static string[] userPasswords = { "1234", "abcd", "qwerty" };
+{
+    static string[] userNames = { "Pelle", "Stina", "Ali" };
+    static string[] userPasswords = { "1234", "12345", "12346" };
 
         static void Main(string[] args)
         {
+            Console.WriteLine("Hej");
+            Thread.Sleep(3000);
+            bool runProgram = true;
+            while (runProgram)
+            {
+                Console.WriteLine("Välj 0 eller 1");
+
+                if (int.TryParse(Console.ReadLine(), out int choice))
+                {
+                    if (choice == 1)
+                    {
+                        Console.WriteLine("Du valde 1");
+                    }
+                    else if (choice == 0)
+                    {
+                        runProgram = false;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Välj i menyn");
+                    {
+                        Console.WriteLine("Hej då");
+                        Thread.Sleep(3000);
+                    }
+                }
+            }
         }
+     
         static void AddUser()
         {
             Console.WriteLine("Hello from AddUser");
@@ -22,19 +46,30 @@ namespace SecretApp1
             Console.WriteLine("Hello from ChangePassword");
         }
 
-        static void ShowUsers()
+    static void ShowUsers()
+    {
+        int i = 0;
+        while (i < userNames.Length)
         {
-            int i = 0;
-            while (i < userNames.Length)
-            {
-                Console.WriteLine(userNames[i].ToUpper());
-            }
-
-        }
-
-        static void EndApplication()
-        {
-            Console.WriteLine("Hello from EndApplication");
+            Console.WriteLine(userNames[i].ToUpper());
+            i++;
         }
     }
+
+    static void LoggIn()
+    {
+        Console.WriteLine("Helo from LoggIn");
+    }
+
+
+    static void ChangePassword()
+    {
+        Console.WriteLine("Hello from ChangePassword()");
+    }
+
+    static void EndApplication()
+    {
+        Console.WriteLine("Hello from EndApplication()");
+    }
+}
 }
